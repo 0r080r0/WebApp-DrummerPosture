7486b42e0c28b4c9c01bbbbd150e17f00a4deaeb
Evangelia Karakoliou <145946571+0r080r0@users.noreply.github.com>
2025-06-09 13:52:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// pass a function to start measuring performance in the app
+// to log results (e.g.: reportWebVitals(console.log))
+// or send to an analytics endpoint (see: https://bit.ly/CRA-vitals)
 reportWebVitals();
